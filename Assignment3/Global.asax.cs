8e43fd348048e59daa3f5743d29285a5731d9f1d
jhou2 <hou.joseph@gmail.com>
2014-03-29 17:49:52 -0700

    global asax database

--- Assignment3/Global.asax.cs
+++ Assignment3/Global.asax.cs
@@ -18,7 +18,7 @@ namespace Assignment3
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
+            Database.SetInitializer<DiplomaContext>(null);
         }
     }
 }
